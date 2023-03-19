ddd453ddb5f999b12f84e2f0e9a726a400f5d426
Muhammed <muhammednkazi@gmail.com>
2023-03-19 15:44:37 +0530

    updated index

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Everyone World');
+  res.send('Hello Everyone World One');
 });
 
 app.listen(PORT, HOST, () => {
